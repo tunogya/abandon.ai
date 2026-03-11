5a713d097b4176c6c873f356117ccda8d1aa391c
tunogya <tunogya@qq.com>
2026-03-12 02:39:32 +0800

    feat: integrate Google Analytics for tracking user interactions

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import { GeistPixelSquare, GeistPixelCircle, GeistPixelLine, GeistPixelTriangle, GeistPixelGrid } from "geist/font/pixel";
 import { Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistMono = Geist_Mono({
@@ -17,6 +18,8 @@ export const metadata: Metadata = {
   },
 };
 
+const GA_MEASUREMENT_ID = "G-DWH7P05G35";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -27,7 +30,18 @@ export default function RootLayout({
       lang="en"
       className={`${geistMono.variable} ${GeistPixelSquare.variable} ${GeistPixelCircle.variable} ${GeistPixelLine.variable} ${GeistPixelTriangle.variable} ${GeistPixelGrid.variable}`}
     >
-      <body>{children}</body>
+      <body>
+        <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   );
 }
